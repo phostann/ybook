a5a6027b87da85036103365f742c91b7ee4b3054
phostann <yl20181003@gmail.com>
2025-09-06 17:32:30 +0800

    chore(security): move PasswordConfig to security package for cohesion

--- src/main/java/com/example/ybook/security/PasswordConfig.java
+++ src/main/java/com/example/ybook/security/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.example.ybook.config;
+package com.example.ybook.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
