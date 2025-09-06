27706de78f88c478a5575e03681315d5d21d8af7
phostann <yl20181003@gmail.com>
2025-09-07 00:12:55 +0800

    chore: 暂存代码

--- src/main/java/com/example/ybook/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/ybook/handler/GlobalExceptionHandler.java
@@ -24,6 +24,7 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.stream.Collectors;
 
